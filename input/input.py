6cd6688e5e3d9eed7446664275af03673b99e8a0
indigobuffalo <stephen.akerson@gmail.com>
2023-09-23 22:44:30 -0400

    update per draft picks

--- input/input.py
+++ input/input.py
@@ -1,27 +1,5 @@
 kkupfl_players = [
-    'Artemi Panarin',
-    'Zach Hyman',
-    'Alex Tuch',
-    '^Alex.* Debrin',
-    '^Ryan Nuge.*',
-    '^J.* Carlson',
-    'Nico His.*'
-    # '^D.* Hamil',
-    # '^Ra.* Dahl',
-    # '^Set.* Jone',
-    # '^Bre.* Burn',
-    # '^Qui.* Hugh',
-    # '^Vic.* Hed',
-    # '^Ale.* Pie',
 ]
 
 pa_players = [
-    '.* Hamil',
-    'R.* Josi',
-    'Do.* Hamil',
-    'Al.* Bark',
-    'Ste.* Stamk',
-    'Eva.* Bouch',
-    'Mor.* Sei',
-    'Andr.* Svech',
 ]
